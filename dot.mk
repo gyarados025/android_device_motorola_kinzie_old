a4e5b847e2e25165dfec1ee4dc5e26345322031a
Kshitij Kumar <40200770+gyarados025@users.noreply.github.com>
2018-07-16 13:18:47 +0530

    DOT

--- dot.mk
+++ dot.mk
@@ -4,13 +4,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/kinzie/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/kinzie/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_kinzie
+PRODUCT_NAME := dot_kinzie
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := kinzie
 PRODUCT_MODEL := XT1581
