34db18149b3c2f3557f28ad1a799a9276145a0ab
Kshitij Kumar <40200770+gyarados025@users.noreply.github.com>
2018-07-17 08:59:18 +0530

    Revert Fingerprint and Description

--- dot.mk
+++ dot.mk
@@ -11,12 +11,12 @@ DEVICE_PACKAGE_OVERLAYS += device/motorola/kinzie/overlay
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := dot_kinzie
-PRODUCT_BRAND := motorola
+PRODUCT_BRAND := Android
 PRODUCT_DEVICE := kinzie
 PRODUCT_MODEL := XT1581
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-BUILD_FINGERPRINT=motorola/kinzie_reteu/kinzie:7.0/NPKS25.200-12-9/6:user/release-keys \
-PRIVATE_BUILD_DESC="kinzie_reteu-user 7.0 NPKS25.200-12-9 6 release-keys"
+##PRODUCT_BUILD_PROP_OVERRIDES += \
+##BUILD_FINGERPRINT=motorola/kinzie_reteu/kinzie:7.0/NPKS25.200-12-9/6:user/release-keys \
+##PRIVATE_BUILD_DESC="kinzie_reteu-user 7.0 NPKS25.200-12-9 6 release-keys"
